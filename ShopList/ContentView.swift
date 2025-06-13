977811b44b0c1c6dedd2fd95b62307536c614b9b
Animesh Naskar <animesh303@gmail.com>
2025-06-13 23:28:40 +0530

    Initial Commit

---
+++ ShopList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopList
+//
+//  Created by Animesh Naskar on 13/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShopList/ShopListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopListApp.swift
+//  ShopList
+//
+//  Created by Animesh Naskar on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
